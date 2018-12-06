468c04926b989298ed70fa330d3165db51716070
Paul <paul58914080@gmail.com>
2018-12-06 07:44:49 +0530

    Fixed Failed to construct 'HTMLElement': Please use the 'new' operator

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,3 +78,13 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/***************************************************************************************************
+ * CUSTOM ELEMENTS
+ */
+
+// Used for browsers with partially native support of Custom Elements
+import '@webcomponents/custom-elements/src/native-shim';
+
+// Used for browsers without a native support of Custom Elements
+import '@webcomponents/custom-elements/custom-elements.min';
