973c0a05e8ec09eda4c41b737c0c52facc3e1205
Paul <paul58914080@gmail.com>
2018-12-06 07:30:00 +0530

    Changed the encapsulation to shadowdom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
-  title = 'angular-elements-demo';
+  @Input() title = 'angular-elements-demo';
 }
